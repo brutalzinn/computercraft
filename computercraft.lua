d349adeed901c9c7d460a0b37ca958ba59c9dfbc
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-06 17:56:06 -0300

    no message

--- computercraft.lua
+++ computercraft.lua
@@ -4,6 +4,15 @@ local chars = { }
 local backchar = { }
 local rechar = { }
 local waiting = false
+function checkfuel()
+turtle.select(1)
+	if turtle.getItemCount(1) > 10 then
+	turtle.refuel()
+	else
+	recharge()
+	end
+
+end
 function recharge()
 for i = 1, #rechargeBase do
 rechar[#rechar + 1] = rechargeBase:sub(i,i)
@@ -31,16 +40,7 @@ end
 end
 
 end
-function checkfuel()
-turtle.select(1)
-if turtle.getFuelLevel() ~= "unlimited" and turtle.getFuelLevel() < 1 then
-	if turtle.getItemCount(1) > 10 then
-	turtle.refuel()
-	else
-	recharge()
-	end
-end
-end
+
 function startMove()
 for i = 1, #mystring do
 chars[#chars + 1] = mystring:sub(i,i)
@@ -99,7 +99,7 @@ end
 function selectItem(name)
    -- check all inventory slots
    local item
-   for slot = 1, 16 do
+   for slot = 2, 16 do
      item = turtle.getItemDetail(slot)
      if item ~= nil and item['name'] == name then
        turtle.select(slot)
