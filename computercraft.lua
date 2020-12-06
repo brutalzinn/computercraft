3c9f992abb765ebff066381f76e5492452b83d55
DESKTOP-4DV71MT\rober <gordododelphi@gmail.com>
2020-12-05 21:43:17 -0300

    commit

--- computercraft.lua
+++ computercraft.lua
@@ -1,13 +1,14 @@
-local mystring = "BB"
+local mystring = "RFFF"
 local chars = { }
-
+local backchar = { }
+local waiting = false
 function checkfuel()
 turtle.select(1)
 if turtle.refuel(0) then
 local halfStack = math.ceil(turtle.getItemCount(i)/2)
 end
 end
-
+function startMove()
 for i = 1, #mystring do
 chars[#chars + 1] = mystring:sub(i,i)
 vetor = chars[#chars]
@@ -24,5 +25,77 @@ end
 if vet == "B" then
 checkfuel()
 turtle.back()
+end
+end
+end
+function backHome()
+local backstring = string.reverse(mystring)
+for i = 1, #backstring do
+backchar[#backchar + 1] = backstring:sub(i,i)
+if vet == "L" then
+turtle.turnLeft()
+if i == 1 then
+turtle.turnLeft()
+end
+end
+if vet == "R" then
+turtle.turnRight()
+if i == 1 then
+turtle.turnRight()
+end
+end
+if vet == "F" then
+checkrefuel()
+turtle.back()
+
+end
+if vet == "B" then
+checkfuel()
+turtle.forward()
+end
+end
+function selectItem(name)
+   -- check all inventory slots
+   local item
+   for slot = 1, 16 do
+     item = turtle.getItemDetail(slot)
+     if item ~= nil and item['name'] == name then
+       turtle.select(slot)
+       return true
+    end
+   end
+
+  return false  -- couldn't find item
+ end
+  function selectEmptySlot()
+   -- loop through all slots
+   for slot = 1, 16 do  
+     if turtle.getItemCount(slot) == 0 then
+       turtle.select(slot)
+       return true
+     end
+  end
+   return false -- couldn't find empty space
+ end
+ function WaitForge()
+ while waiting do
+ 
+ if turtle.suck() then
+ waiting = false;
+ break;
+ end
+ end
+ 
+ end
+function init()
+rednet.open("right")
+local sender, message, protocol = rednet.receive()
+for 1, tonumber(message) do
+selectEmptySlot()
+WaitForge()
+startMove()
+turtle.drop()
+backHome()
+sleep(0.1)
 end
 end
